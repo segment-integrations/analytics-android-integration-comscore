02879ec404f4dd82695fa64091d9b90ee29b26f8
ladan nasserian <ladannasserian@gmail.com>
2017-03-29 15:16:18 -0700

    Adds video tracking, WIP tests video tracking

--- src/main/java/com/segment/analytics/android/integrations/comscore/ComScoreIntegration.java
+++ src/main/java/com/segment/analytics/android/integrations/comscore/ComScoreIntegration.java
@@ -3,6 +3,8 @@ package com.segment.analytics.android.integrations.comscore;
 import com.comscore.Analytics;
 import com.comscore.PublisherConfiguration;
 import com.comscore.UsagePropertiesAutoUpdateMode;
+import com.comscore.streaming.PlaybackSession;
+import com.comscore.streaming.StreamingAnalytics;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.IdentifyPayload;
 import com.segment.analytics.integrations.Integration;
@@ -10,33 +12,49 @@ import com.segment.analytics.integrations.Logger;
 import com.segment.analytics.integrations.ScreenPayload;
 import com.segment.analytics.integrations.TrackPayload;
 import com.comscore.PartnerConfiguration;
+import com.segment.analytics.internal.Utils;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class ComScoreIntegration extends Integration<Void> {
-  public static final Factory FACTORY =
-      new Factory() {
-        @Override
-        public Integration<?> create(ValueMap settings, com.segment.analytics.Analytics analytics) {
-          return new ComScoreIntegration(analytics, settings);
-        }
 
-        @Override
-        public String key() {
-          return COMSCORE_KEY;
-        }
-      };
+  public static final Factory FACTORY = new Factory() {
+    @Override
+    public Integration<?> create(ValueMap settings, com.segment.analytics.Analytics analytics) {
+      return new ComScoreIntegration(analytics, settings, StreamingAnalyticsFactory.REAL);
+    }
+
+    @Override public String key() {
+      return COMSCORE_KEY;
+    }
+  };
 
   private static final String COMSCORE_KEY = "comScore";
   final Logger logger;
-  String customerC2;
-  String publisherSecret;
-  String appName;
-  boolean useHTTPS;
-  int autoUpdateInterval;
-  boolean autoUpdate;
-  boolean foregroundOnly;
+  final StreamingAnalyticsFactory streamingAnalyticsFactory;
+  final String customerC2;
+  final String publisherSecret;
+  final String appName;
+  final boolean useHTTPS;
+  final int autoUpdateInterval;
+  final boolean autoUpdate;
+  final boolean foregroundOnly;
+  public StreamingAnalytics streamingAnalytics;
 
-  ComScoreIntegration(com.segment.analytics.Analytics analytics, ValueMap settings) {
+  interface StreamingAnalyticsFactory {
+    StreamingAnalytics create();
+
+    StreamingAnalyticsFactory REAL = new StreamingAnalyticsFactory() {
+      @Override public StreamingAnalytics create() {
+        return new StreamingAnalytics();
+      }
+    };
+  }
+
+  ComScoreIntegration(com.segment.analytics.Analytics analytics, ValueMap settings,
+      StreamingAnalyticsFactory streamingAnalyticsFactory) {
+    this.streamingAnalyticsFactory = streamingAnalyticsFactory;
     logger = analytics.logger(COMSCORE_KEY);
     customerC2 = settings.getString("c2");
     publisherSecret = settings.getString("publisherSecret");
@@ -73,17 +91,116 @@ public class ComScoreIntegration extends Integration<Void> {
     Analytics.start(analytics.getApplication());
   }
 
-  @Override
-  public void track(TrackPayload track) {
+  @Override public void track(TrackPayload track) {
     String name = track.event();
-    HashMap<String, String> properties = (HashMap<String, String>) track.properties().toStringMap();
-    properties.put("name", name);
-    Analytics.notifyHiddenEvent(properties);
-    logger.verbose("Analytics.hidden(%s)", properties);
+    Map<String, String> properties = track.properties().toStringMap();
+
+    Map<String, String> playbackMapper = new LinkedHashMap<>();
+    playbackMapper.put("asset_id", "ns_st_ci");
+    playbackMapper.put("ad_type", "ns_st_ad");
+    playbackMapper.put("length", "nst_st_cl");
+    playbackMapper.put("video_player", "ns_st_st");
+
+
+    Map<String, String> contentMapper = new LinkedHashMap<>();
+    contentMapper.put("asset_id", "ns_st_ci");
+    contentMapper.put("title", "ns_st_pr");
+    contentMapper.put("keywords", "ns_st_ge");
+    contentMapper.put("season", "ns_st_sn");
+    contentMapper.put("episode", "ns_st_ep");
+    contentMapper.put("genre", "ns_st_ge");
+    contentMapper.put("program", "ns_st_pr");
+    contentMapper.put("channel", "ns_st_pu");
+    contentMapper.put("full_episode", "ns_st_ce");
+    contentMapper.put("airdate", "ns_st_ddt");
+
+    Map<String, String> adMapper = new LinkedHashMap<>();
+    adMapper.put("asset_id", "ns_st_ci");
+    adMapper.put("pod_id","ns_st_pn");
+    adMapper.put("type", "ns_st_ad");
+    adMapper.put("publisher", "ns_st_pu");
+    adMapper.put("length","ns_st_cl");
+
+    Map<String, String> playbackAsset = Utils.transform(properties, playbackMapper);
+    Map<String, String> contentAsset = Utils.transform(properties, contentMapper);
+    Map<String, String> adAsset = Utils.transform(properties, adMapper);
+
+    switch (name) {
+      case "Video Playback Started":
+        streamingAnalytics = streamingAnalyticsFactory.create();
+        streamingAnalytics.createPlaybackSession();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Playback Paused":
+        streamingAnalytics.notifyPause();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Playback Buffer Started":
+        streamingAnalytics.notifyBufferStart();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Playback Buffer Completed":
+        streamingAnalytics.notifyBufferStop();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Playback Seek Started":
+        streamingAnalytics.notifySeekStart();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Playback Seek Completed":
+        streamingAnalytics.notifyEnd();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Playback Resumed":
+        streamingAnalytics.notifyPlay();
+        streamingAnalytics.getPlaybackSession().setAsset(playbackAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", playbackAsset);
+        break;
+
+      case "Video Content Started":
+        streamingAnalytics.notifyPlay();
+        streamingAnalytics.getPlaybackSession().setAsset(contentAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", contentAsset);
+        break;
+
+      case "Video Content Completed":
+        streamingAnalytics.notifyEnd();
+        streamingAnalytics.getPlaybackSession().setAsset(contentAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", contentAsset);
+        break;
+
+      case "Video Ad Started":
+        streamingAnalytics.notifyPlay();
+        streamingAnalytics.getPlaybackSession().setAsset(adAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", adAsset);
+        break;
+
+      case "Video Ad Completed":
+        streamingAnalytics.notifyEnd();
+        streamingAnalytics.getPlaybackSession().setAsset(adAsset);
+        logger.verbose("streamingAnalytics.getPlaybackSession().setAsset(%s)", adAsset);
+        break;
+
+      default:
+        properties.put("name", name);
+        Analytics.notifyHiddenEvent(properties);
+        logger.verbose("Analytics.hidden(%s)", properties);
+    }
   }
 
-  @Override
-  public void identify(IdentifyPayload identify) {
+  @Override public void identify(IdentifyPayload identify) {
     super.identify(identify);
     String userId = identify.userId();
     HashMap<String, String> traits = (HashMap<String, String>) identify.traits().toStringMap();
@@ -92,13 +209,11 @@ public class ComScoreIntegration extends Integration<Void> {
     logger.verbose("Analytics.setPersistentLabels(%s)", traits);
   }
 
-  @Override
-  public void screen(ScreenPayload screen) {
+  @Override public void screen(ScreenPayload screen) {
     String name = screen.name();
     String category = screen.category();
-    HashMap<String, String> properties =
-        (HashMap<String, String>) //
-            screen.properties().toStringMap();
+    HashMap<String, String> properties = (HashMap<String, String>) //
+        screen.properties().toStringMap();
     properties.put("name", name);
     properties.put("category", category);
     Analytics.notifyViewEvent(properties);
--- src/test/java/com/segment/analytics/android/integrations/comscore/ComScoreTest.java
+++ src/test/java/com/segment/analytics/android/integrations/comscore/ComScoreTest.java
@@ -7,16 +7,20 @@ import com.comscore.PartnerConfiguration;
 import com.comscore.PublisherConfiguration;
 import com.comscore.UsagePropertiesAutoUpdateMode;
 import com.comscore.Analytics;
+import com.comscore.streaming.PlaybackSession;
+import com.comscore.streaming.StreamingAnalytics;
 import com.segment.analytics.Properties;
 import com.segment.analytics.Traits;
 import com.segment.analytics.ValueMap;
 import com.segment.analytics.integrations.Logger;
+import com.segment.analytics.integrations.TrackPayload;
 import com.segment.analytics.test.IdentifyPayloadBuilder;
 import com.segment.analytics.test.ScreenPayloadBuilder;
 import com.segment.analytics.test.TrackPayloadBuilder;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.maven.artifact.ant.shaded.cli.Arg;
 import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Rule;
@@ -24,14 +28,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.rule.PowerMockRule;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import static org.mockito.BDDMockito.*;
+
 import static com.segment.analytics.Analytics.LogLevel.VERBOSE;
 import static com.segment.analytics.Utils.createTraits;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -39,8 +46,9 @@ import static org.mockito.MockitoAnnotations.initMocks;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.support.membermodification.MemberModifier.replace;
 
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 18, manifest = Config.NONE)
 @PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
 @PrepareForTest(Analytics.class) public class ComScoreTest {
@@ -52,6 +60,7 @@ import static org.powermock.api.mockito.PowerMockito.when;
   @Mock com.segment.analytics.Analytics analytics;
   ComScoreIntegration integration;
   @Mock Analytics comScore;
+  @Mock StreamingAnalytics streamingAnalytics;
 
   @Before public void setUp() {
     initMocks(this);
@@ -62,7 +71,12 @@ import static org.powermock.api.mockito.PowerMockito.when;
     when(analytics.getApplication()).thenReturn(context);
     integration = new ComScoreIntegration(analytics,
         new ValueMap().putValue("customerC2", "foobarbar")
-            .putValue("publisherSecret", "illnevertell"));
+            .putValue("publisherSecret", "illnevertell"),
+        new ComScoreIntegration.StreamingAnalyticsFactory() {
+          @Override public StreamingAnalytics create() {
+            return streamingAnalytics;
+          }
+        });
 
     // mock it twice so we can initialize it for tests, but reset the mock after initialization.
     mockStatic(Analytics.class);
@@ -106,8 +120,7 @@ import static org.powermock.api.mockito.PowerMockito.when;
         .putValue("useHTTPS", true)
         .putValue("autoUpdateInterval", 2000)
         .putValue("autoUpdate", true)
-        .putValue("foregroundOnly", true));
-
+        .putValue("foregroundOnly", true), null);
 
     ArgumentCaptor<ClientConfiguration> configurationCaptor =
         ArgumentCaptor.forClass(ClientConfiguration.class);
@@ -115,14 +128,15 @@ import static org.powermock.api.mockito.PowerMockito.when;
 
     List<ClientConfiguration> capturedConfig = configurationCaptor.getAllValues();
     assertThat(((PartnerConfiguration) capturedConfig.get(0)).getPartnerId()).isEqualTo("24186693");
-    assertThat(((PublisherConfiguration) capturedConfig.get(1)).getPublisherId()).isEqualTo("foobarbar");
-    assertThat(((PublisherConfiguration) capturedConfig.get(1)).getPublisherSecret()).isEqualTo("illnevertell");
+    assertThat(((PublisherConfiguration) capturedConfig.get(1)).getPublisherId()).isEqualTo(
+        "foobarbar");
+    assertThat(((PublisherConfiguration) capturedConfig.get(1)).getPublisherSecret()).isEqualTo(
+        "illnevertell");
     assertThat(capturedConfig.get(1).getApplicationName()).isEqualTo("testApp");
     assertThat(capturedConfig.get(1).getUsagePropertiesAutoUpdateInterval()).isEqualTo(2000);
     assertThat(capturedConfig.get(1).getUsagePropertiesAutoUpdateMode()).isEqualTo(
         UsagePropertiesAutoUpdateMode.FOREGROUND_AND_BACKGROUND);
-  };
-
+  }
 
   @Test public void track() {
     integration.track(new TrackPayloadBuilder().event("foo").build());
@@ -147,6 +161,56 @@ import static org.powermock.api.mockito.PowerMockito.when;
 
     verifyStatic();
     Analytics.notifyHiddenEvent(expected);
+  }
+
+  @Test public void videoPlaybackStarted() {
+    PlaybackSession playbackSession = mock(PlaybackSession.class);
+    when(streamingAnalytics.getPlaybackSession()).thenReturn(playbackSession);
+
+    integration.track(new TrackPayloadBuilder().event("Video Playback Started")
+        .properties(new Properties()
+            .putValue("asset_id", "1234")
+            .putValue("ad_type", "pre-roll")
+            .putValue("length", "120")
+            .putValue("video_player", "youtube"))
+        .build());
+
+    verify(streamingAnalytics).createPlaybackSession();
+    verify(streamingAnalytics).getPlaybackSession();
+
+    LinkedHashMap<String, String> expected = new LinkedHashMap<>();
+    expected.put("ns_st_ci", "1234");
+    expected.put("ns_st_ad", "pre-roll");
+    expected.put("nst_st_cl", "120");
+    expected.put("ns_st_st", "youtube");
+
+    verify(playbackSession).setAsset(expected);
+  }
+
+  @Test public void videoPlaybackPaused() {
+    PlaybackSession playbackSession = mock(PlaybackSession.class);
+    when(streamingAnalytics.getPlaybackSession()).thenReturn(playbackSession);
+
+    integration.streamingAnalytics = streamingAnalytics;
+
+    integration.track(new TrackPayloadBuilder().event("Video Playback Paused")
+        .properties(new Properties()
+            .putValue("asset_id", "1234")
+            .putValue("ad_type", "mid-roll")
+            .putValue("length", "100")
+            .putValue("video_player", "vimeo"))
+        .build());
+
+    verify(streamingAnalytics).notifyPause();
+    verify(streamingAnalytics).getPlaybackSession();
+
+    LinkedHashMap<String, String> expected = new LinkedHashMap<>();
+    expected.put("ns_st_ci", "1234");
+    expected.put("ns_st_ad", "mid-roll");
+    expected.put("nst_st_cl", "100");
+    expected.put("ns_st_st", "vimeo");
+
+    verify(playbackSession).setAsset(expected);
   }
 
   @Test public void identify() throws JSONException {
